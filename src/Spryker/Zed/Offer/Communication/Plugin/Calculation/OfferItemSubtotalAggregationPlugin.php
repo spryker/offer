90a70264bd66587c24ac39720ec4a3d8042d60aa
Denis Turkov <denis.turkov@spryker.com>
2018-04-09 20:43:46 +0000

    CORE-3784: Move a plugin to offer

---
+++ src/Spryker/Zed/Offer/Communication/Plugin/Calculation/OfferItemSubtotalAggregationPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Offer\Communication\Plugin\Calculation;
+
+use Generated\Shared\Transfer\CalculableObjectTransfer;
+use Spryker\Zed\Calculation\Dependency\Plugin\CalculationPluginInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \Spryker\Zed\Offer\Business\OfferFacadeInterface getFacade()
+ */
+class OfferItemSubtotalAggregationPlugin extends AbstractPlugin implements CalculationPluginInterface
+{
+    /**
+     * Guide: This calculator plugin must be placed under ItemSubtotalAggregatorPlugin
+     * after the generic logic is executed.
+     *
+     * @param \Generated\Shared\Transfer\CalculableObjectTransfer $calculableObjectTransfer
+     *
+     * @return void
+     */
+    public function recalculate(CalculableObjectTransfer $calculableObjectTransfer)
+    {
+         $this->getFacade()->aggregateOfferItemSubtotal($calculableObjectTransfer);
+    }
+}
